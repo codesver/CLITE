6f2d5821f56d969cc5ed315dff464716ee783511
JaeWon Choi <unsyg.choi@gmail.com>
2022-06-01 09:55:58 +0900

    [Update] StaticTypeCheck|TypeMap|TypeTransformer JW06010955
    - Implement

--- code/src/TypeMap.java
+++ code/src/TypeMap.java
@@ -4,5 +4,7 @@ public class TypeMap extends HashMap<Variable, Type> {
 
 // TypeMap is implemented as a Java HashMap.  
 // Plus a 'display' method to facilitate experimentation.
-
+    public void display() {
+        System.out.println(this.entrySet());
+    }
 }
