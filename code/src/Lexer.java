102f49132b5544f83c0cdb37891e73082e698ed7
JaeWon Choi <unsyg.choi@gmail.com>
2022-06-01 09:50:40 +0900

    [Create] CLITE JW06010950
    - Create Clite file

---
+++ code/src/Lexer.java
@@ -0,0 +1,148 @@
+import java.io.*;
+
+public class Lexer {
+
+    private boolean isEof = false;
+    private char ch = ' '; 
+    private BufferedReader input;
+    private String line = "";
+    private int lineno = 0;
+    private int col = 1;
+    private final String letters = "abcdefghijklmnopqrstuvwxyz"
+        + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final String digits = "0123456789";
+    private final char eolnCh = '\n';
+    private final char eofCh = '\004';
+    
+
+    public Lexer (String fileName) { // source filename
+        try {
+            input = new BufferedReader (new FileReader(fileName));
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
+            System.exit(1);
+        }
+    }
+
+    /* 한 줄을 한 문자씩 처리 */
+    private char nextChar() { // Return next char
+        if (ch == eofCh)
+            error("Attempt to read past end of file");
+        col++;
+        if (col >= line.length()) {
+            try {
+                line = input.readLine( );
+            } catch (IOException e) {
+                System.err.println(e);
+                System.exit(1);
+            } // try
+            if (line == null) // at end of file
+                line = "" + eofCh;
+            else {
+                // System.out.println(lineno + ":\t" + line);
+                lineno++;
+                line += eolnCh;
+            } // if line
+            col = 0;
+        } // if col
+        return line.charAt(col);
+    }
+            
+    /* Important */
+    public Token next( ) { // Return next token
+        do {
+            if (isLetter(ch)) { // identifier or keyword
+                String spelling = concat(letters + digits); // Letter나 Digit이 나오면 계속해서 concat
+                return Token.keyword(spelling);
+            } else if (isDigit(ch)) { // int or float literal
+                String number = concat(digits);
+                if (ch != '.')  // int Literal
+                    return Token.mkIntLiteral(number);
+                number += concat(digits);
+                return Token.mkFloatLiteral(number);
+            } else switch (ch) {
+            case ' ': case '\t': case '\r': case eolnCh:        // Whitespace
+                ch = nextChar();
+                break;
+            
+            case '/':  // divide or comment
+                ch = nextChar();
+                if (ch != '/')  return Token.divideTok;
+                // comment
+                do {
+                    ch = nextChar();
+                } while (ch != eolnCh);
+                ch = nextChar();
+                break;
+            
+            case '\'':  // char literal
+                char ch1 = nextChar();
+                nextChar(); // get '
+                ch = nextChar();
+                return Token.mkCharLiteral("" + ch1);
+                
+            case eofCh: return Token.eofTok;
+            
+            case '+': ch = nextChar();
+                return Token.plusTok;
+
+                // - * ( ) { } ; ,  student exercise 
+                
+            case '&': check('&'); return Token.andTok;
+            case '|': check('|'); return Token.orTok;
+
+            case '=':
+                return chkOpt('=', Token.assignTok,
+                                   Token.eqeqTok);
+                // < > !  student exercise 
+
+            default:  error("Illegal character " + ch); 
+            } // switch
+        } while (true);
+    } // next
+
+
+    private boolean isLetter(char c) {
+        return (c>='a' && c<='z' || c>='A' && c<='Z');
+    }
+  
+    private boolean isDigit(char c) {
+        return false;  // student exercise 0 ~ 9
+    }
+
+    private void check(char c) {
+        ch = nextChar();
+        if (ch != c) 
+            error("Illegal character, expecting " + c);
+        ch = nextChar();
+    }
+
+    private Token chkOpt(char c, Token one, Token two) {
+        return null;  // student exercise c가 아니면 one c이면 two
+    }
+
+    private String concat(String set) {
+        String r = "";
+        do {
+            r += ch;
+            ch = nextChar();
+        } while (set.indexOf(ch) >= 0);
+        return r;
+    }
+
+    public void error (String msg) {
+        System.err.print(line);
+        System.err.println("Error: column " + col + " " + msg);
+        System.exit(1);
+    }
+
+    static public void main ( String[] argv ) {
+        Lexer lexer = new Lexer(argv[0]);
+        Token tok = lexer.next( );
+        while (tok != Token.eofTok) {
+            System.out.println(tok.toString());
+            tok = lexer.next( );
+        } 
+    } // main
+}
